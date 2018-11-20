3bbb05cfd5f7c44a72af4acd5243f65380c0d04d
Taewoo Kim <boratw54@gmail.com>
2018-11-20 17:21:22 +0900

    IIT6028-hw4-final

--- IIT6028/hw4/getw.m
+++ IIT6028/hw4/getw.m
@@ -1,11 +1,12 @@
 
 function ret = getw(v)
-%     ret = 1;
     if((v == 0) || (v == 255))
         ret = 0;
     else
-       ret = exp(-((double(v) - 128.) ^ 2) / 4096);
-    	ret = (128. - abs(double(v) - 128.)) / 128.;
+        %select weighting function and uncomment one
+        
+        %ret = exp(-((double(v) - 128.) ^ 2) / 4096);
+    	%ret = (128. - abs(double(v) - 128.)) / 128.;
         ret = 1;
    end
 end
